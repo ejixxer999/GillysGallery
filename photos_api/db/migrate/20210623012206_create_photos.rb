97427e73e2c1149b031e4ddd84c353ec0e8aa432
ejixxer999 <lehman.ethan2@gmail.com>
2021-06-23 19:45:03 -0400

    changed

--- photos_api/db/migrate/20210623012206_create_photos.rb
+++ photos_api/db/migrate/20210623012206_create_photos.rb
@@ -2,7 +2,8 @@ class CreatePhotos < ActiveRecord::Migration[6.1]
   def change
     create_table :photos do |t|
       t.string :date
-      t.string :author
+      t.string :photographer
+      t.string :img_url
       t.integer :gallery_id
 
       t.timestamps
