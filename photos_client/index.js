a2722109ada8cea9bdbdbe78552c19ce1e40329c
ejixxer999 <lehman.ethan2@gmail.com>
2021-07-05 19:09:26 -0400

    added show function, handleSubmit, back button, and delete photo functions

--- photos_client/index.js
+++ photos_client/index.js
@@ -1,34 +1,62 @@
-const endPoint = "https://localhost:3000/photos"
+const base_url = "http://127.0.0.1:3000"
+const photoService = new PhotoService(base_url)
+const photoContainer = document.querySelector('#photos-container')
 
-class Photos {
-    constructor(photo, photoAttributes){
-        this.id = photo.id;
-        this.date = photoAttributes.date;
-        this.image_url = photoAttributes.image_url;
-        this.photographer = photoAttributes.photographer;
+Photo.photoForm.addEventListener('submit', handleSubmit)
+photoService.getPhotos()
 
-        Photos.call.push(this);
-    }
+Photo.renderForm()
 
-    renderPhoto(){
-        return `
-        <div data-id=${this.id}>
-        <img src=${this.image_url} height="300" width="200">
-        <p>${this.photographer}</p>
-        <p>${this.date}</p>
-        </div>
-        <br><br>`;
-
-    }
+function handleSubmit(event){
+    event.preventDefault()
+    photoService.createPhoto()
+    photoService.resetForm()
 }
 
-Photos.all = [];
+function photoShow(){
+    const dev = event.target.parentElement.dataset.id
+    fetch(`http://127.0.0.1:3000/photos/${dev}`)
+    .then(resp => resp.json())
+    .then(photo => {
+        photoContainer.innerHTML = ''
+        photoContainer.innerHTML += `
+        <img src=${photo.image} />
+        <br>
+        <a id="backbttn" href="#">Back</a>
+        `
+        
+        const backbttn = document.getElementById('backbttn')
+        backbttn.addEventListener('click', back)
 
-function getPhotos(){
-    fetch(endPoint)
-    .then(response => response.json())
-    .then(photos => {photos.data.forEach(photo => {let newPhoto = newPhoto(photo,photo.photoAttributes)
-    document.querySelector('#photos-container').innerHTML += newPhoto.renderPhoto()
     })
-})
 }
+
+
+function back(){
+    photoContainer.innerHTML = ''
+    photoService.getPhotos()
+}
+
+function handleButton(event){
+    if (event.target.innerText === 'Delete'){
+       
+        deletePhoto(event.target)
+
+    } else if (event.target.innerText === 'Edit'){
+        editPhoto(event.target)
+
+    }
+
+}
+
+function deletePhoto(element){
+   element.parentElement.remove()
+   const dev = element.parentElement.dataset.id
+   fetch(`http://127.0.0.1:3000/photos/${dev}`, {
+       method: 'DELETE'
+   })
+   .then(resp => resp.json)
+   .then(data => {})
+
+}
+
