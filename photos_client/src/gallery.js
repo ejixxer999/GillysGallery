1645ff865a056cfe066f70a2a0470f2131d2462a
ejixxer999 <lehman.ethan2@gmail.com>
2021-07-08 20:59:04 -0400

    created gallery class and constructor

--- photos_client/src/gallery.js
+++ photos_client/src/gallery.js
@@ -1,19 +1,67 @@
-class Gallery{
+class Gallery {
+    static all = []
+    static galleryForm = document.getElementById('form-area2')
+    static galleryContainer = document.getElementById('gallery-container')
 
-    static all =[]
-    static photoForm = document.getElementById('form-area')
-
-    constructor({name}){
+    constructor({name, id}){
         this.name = name
+        this.id = id
+        this.element = document.createElement('li')
+        this.element.dataset.id = `${this.id}`
+        this.element.id = `gallery-${this.id}`
         Gallery.all.push(this)
+        
     }
 
-    static renderCollection(){
-        Gallery.photoForm.innerHTML += `
-        Gallery: <input type="select" id="gallery">
+    appendToDOM(){
+        Gallery.galleryContainer.append(this.galleryHTML())
+    }   
+
+    static renderGalleryForm(){
+        Gallery.galleryForm.innerHTML += `
+        <form id="new_gallery">
+        Gallery: <input type="text" id="gallery">
+        <input type="submit" id="Publish">
+        </form>
         `
-
     }
+
+    galleryHTML(){
+        this.elememt.innerHTML += `
+        <div data-id=${this.id}>
+            <title>${this.name}</title>
+        </div>
+        `
+        return this.element
+    }
+
+     // photoHTML(){
+    //     this.element.innerHTML += `
+    //         <div data-id=${this.id}>
+    //             <img class="pimage" src=${this.image} height="300" width="200" />
+    //             <h3>${this.date}</h3>
+    //             <p>${this.photographer}</p>
+    //         </div>
+    //             <button id='delete-bttn'>Delete</button>
+    //             `
+        
+    //     const grapherImg = document.getElementsByClassName('pimage')
+    //     for (const img of grapherImg){
+    //         img.addEventListener('click', this.photoShow)    
+    //     }
+    //     // const bttns = document.querySelectorAll('button')
+    //     // for(const button of bttns){
+    //     //     button.addEventListener('click', handleButton)
+    //     // }
+    //     return this.element
+        
+    // }
+ 
+    
+   
+    
+
+   
 
     
 
