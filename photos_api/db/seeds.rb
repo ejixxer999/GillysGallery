f1e9484f62d6d019f660adf19cb395fe214cb563
ejixxer999 <lehman.ethan2@gmail.com>
2021-07-08 20:58:16 -0400

    added seed data

--- photos_api/db/seeds.rb
+++ photos_api/db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-summer = Gallery.create(name: 'summer')
+  summer = Gallery.create(name: 'summer')
 
 Photo.create([
     {date: "2021/05/06", photographer: "gilly", image: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/48457/DIG010147_1._SX360_QL80_TTD_.jpg", gallery: summer},
