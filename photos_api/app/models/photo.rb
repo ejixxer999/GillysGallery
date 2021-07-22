7ba6039390fe75f0f82f62ea11ea24950f994a1d
ejixxer999 <lehman.ethan2@gmail.com>
2021-07-22 09:40:37 -0400

    added the ability for photos to be assigned to a gallery by it's name

--- photos_api/app/models/photo.rb
+++ photos_api/app/models/photo.rb
@@ -1,4 +1,9 @@
 class Photo < ApplicationRecord
   # has_one_attached :image
     belongs_to :gallery
+
+    def gallery_id= (name)
+      gallery = Gallery.find_or_create_by(name: name)
+      self.gallery = gallery
+    end
 end
