a2ba14ad688897182cacc2771dc143277bcfa2c0
ejixxer999 <lehman.ethan2@gmail.com>
2021-07-22 09:39:01 -0400

    can now create a photo through galleries and show photos belonging to that gallery

--- photos_client/src/gallery.js
+++ photos_client/src/gallery.js
@@ -3,67 +3,45 @@ class Gallery {
     static galleryForm = document.getElementById('form-area2')
     static galleryContainer = document.getElementById('gallery-container')
 
-    constructor({name, id}){
+    constructor({name, id, photos}){
         this.name = name
         this.id = id
+        this.photos = photos.map( p => new Photo(p))
         this.element = document.createElement('li')
         this.element.dataset.id = `${this.id}`
         this.element.id = `gallery-${this.id}`
+        this.element.addEventListener('click', this.renderAllPhotos)
         Gallery.all.push(this)
-        
     }
+    
+    renderAllPhotos=() => {
+        console.log('Clicked:', this.photos)
+        photoContainer.innerHTML = ''
+        this.photos.forEach( p => p.slapOnDom())
+    }   
+        // when clicked = clear photosContainer, then replace it with the photos of the gallery
+
 
     appendToDOM(){
         Gallery.galleryContainer.append(this.galleryHTML())
-    }   
+    }
 
     static renderGalleryForm(){
         Gallery.galleryForm.innerHTML += `
         <form id="new_gallery">
-        Gallery: <input type="text" id="gallery">
+        Gallery: <input type="text" id="gallery"><br>
         <input type="submit" id="Publish">
         </form>
         `
     }
 
-    galleryHTML(){
-        this.elememt.innerHTML += `
+    galleryHTML=() => {
+     this.element.innerHTML = `
         <div data-id=${this.id}>
-            <title>${this.name}</title>
+            <p>${this.name}</p>
         </div>
         `
         return this.element
     }
-
-     // photoHTML(){
-    //     this.element.innerHTML += `
-    //         <div data-id=${this.id}>
-    //             <img class="pimage" src=${this.image} height="300" width="200" />
-    //             <h3>${this.date}</h3>
-    //             <p>${this.photographer}</p>
-    //         </div>
-    //             <button id='delete-bttn'>Delete</button>
-    //             `
-        
-    //     const grapherImg = document.getElementsByClassName('pimage')
-    //     for (const img of grapherImg){
-    //         img.addEventListener('click', this.photoShow)    
-    //     }
-    //     // const bttns = document.querySelectorAll('button')
-    //     // for(const button of bttns){
-    //     //     button.addEventListener('click', handleButton)
-    //     // }
-    //     return this.element
-        
-    // }
- 
-    
-   
-    
-
-   
-
-    
-
 
 }
