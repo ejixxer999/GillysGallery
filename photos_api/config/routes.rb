eb7b02a5241709398313a6429a14b9fde8ec3884
ejixxer999 <lehman.ethan2@gmail.com>
2021-07-22 09:39:42 -0400

    added nested routes

--- photos_api/config/routes.rb
+++ photos_api/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :photos
+  resources :photos 
   resources :galleries
+
+  resources :galleries do 
+    resources :photos
+  end 
+
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
