4bccb138ef34da8fc284d4c3cd7d81b5a1ebf0ee
ejixxer999 <lehman.ethan2@gmail.com>
2021-07-22 09:35:52 -0400

    created single fetch request for get, added notes

--- photos_client/index.js
+++ photos_client/index.js
@@ -2,63 +2,37 @@ const base_url = "http://127.0.0.1:3000"
 const photoService = new PhotoService(base_url)
 const galleryService = new GalleryService(base_url)
 const photoContainer = document.querySelector('#photos-container')
+const galleryContainer = document.querySelector('#gallery-container')
+document.addEventListener('DOMContentLoaded',() => {
+    getEverything()
+})
 
 Photo.photoForm.addEventListener('submit', handleSubmit)
-
-photoService.getPhotos()
-galleryService.getGalleries()
-
+//--------------------------render forms-------------//
 Photo.renderForm()
 Gallery.renderGalleryForm()
-
+//-----------Submit Button------------//
 function handleSubmit(event){
     event.preventDefault()
-    // galleryService.createGallery(event)
     photoService.createPhoto(event)
     photoService.resetForm()
 }
-
+    
+//-----------Back Button-----------------//
 function back(){
     photoContainer.innerHTML = ''
-    photoService.getPhotos()
+    galleryContainer.innerHTML = ''
+    getEverything()
+}
+//--------Single fetch for galleries with photos-------------//
+function getEverything(){
+    fetch ('http://127.0.0.1:3000/galleries')
+    .then(resp => resp.json())
+    .then(data => {
+         data.forEach( p => {
+            const d = new Gallery(p)
+            d.appendToDOM()
+             })
+        })
 }
 
-
-// function photoShow(){
-//     const id = event.target.parentElement.dataset.id
-//     fetch(`http://127.0.0.1:3000/photos/${id}`)
-//     .then(resp => resp.json())
-//     .then(photo => {
-//         debugger
-        
-//         photoContainer.innerHTML = ''
-//         photoContainer.innerHTML += `
-//         <img src=${photo.image} />
-//         <br>
-//         <a id="backbttn" href="#">Back</a>
-//         `
-        
-//         const backbttn = document.getElementById('backbttn')
-//         backbttn.addEventListener('click', back)
-
-//     })
-// }
-
-
-
-// function handleButton(event){
-//     if (event.target.innerText === 'Delete'){
-       
-//         photoService.deletePhoto(event.target)
-
-//     } else if (event.target.innerText === 'Edit'){
-//         editPhoto(event.target)
-
-//     }
-
-// }
-//...............TO DO...................//
-// Create a Gallery Form for new galleries
-// create append to dom for galleries 
-// create a handle submit for galleries 
-// Make galleries appear on spa
