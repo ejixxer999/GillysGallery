a907decbf97bd871cc225b314f3577ccc02b5201
ejixxer999 <lehman.ethan2@gmail.com>
2021-07-22 09:36:28 -0400

    get photos replaced by single fetch

--- photos_client/src/photoService.js
+++ photos_client/src/photoService.js
@@ -3,35 +3,9 @@ class PhotoService{
     constructor(endpoint){
         this.endpoint = endpoint
     }
-    getPhotos(){
-        
-        fetch(`${this.endpoint}/photos`)
-        .then(resp => resp.json())
-        .then(photos => {
-            for (const photo of photos){
-                const p = new Photo(photo)
-                p.slapOnDom()
-                
-                
-            }
-            
-        })
-    }
-    //  sending a new image to the backend //
-    // formData(){
-    //    const photo = {
-    //     // id: document.getElementById(`${Photo.element.id}`),
-    //     date: document.getElementById('date').value,
-    //     photographer: document.getElementById('photographer').value,
-    //     image: document.getElementById('photo').value,
-    //     gallery_id: document.getElementById('gallery_id').value
-    //    }
-        
-    // }
-    
+//-----------Send Photos To Back-End-------------//
     createPhoto(event){
         const photo = {
-            // id: document.getElementById(`${Photo.element.id}`),
             date: document.getElementById('date').value,
             photographer: document.getElementById('photographer').value,
             image: document.getElementById('photo').value,
@@ -45,46 +19,21 @@ class PhotoService{
             body: JSON.stringify(photo)
         }
         
-
         fetch(`http://127.0.0.1:3000/photos`, options)
         .then(resp => resp.json())
         .then(photo => {
             const p = new Photo(photo)
             p.slapOnDom()
-            
-        })
-
-           
-        
-    }
-
-     photoShow(){
-        const id = event.target.parentElement.dataset.id
-        fetch(`http://127.0.0.1:3000/photos/${id}`)
-        .then(resp => resp.json())
-        .then(photo => {
-            
-            
-            photoContainer.innerHTML = ''
-            photoContainer.innerHTML += `
-            <img src=${photo.image} />
-            <br>
-            <a id="backbttn" href="#">Back</a>
-            `
-            
-            const backbttn = document.getElementById('backbttn')
-            backbttn.addEventListener('click', back)
-    
         })
     }
-
+//------------Clear Forms After Submit-----------------------//
     resetForm(){
         document.getElementById('date').value = ''
         document.getElementById('photographer').value = ''
         document.getElementById('photo').value = ''
         document.getElementById('gallery_id').value = ''
     }
-
+//-------------Remove Photo-----------------------------//
     deletePhoto(id){
         fetch(`http://127.0.0.1:3000/photos/${id}`, {
             method: 'DELETE',
@@ -94,8 +43,8 @@ class PhotoService{
         })
         .then(resp => resp.json)
         .then(data => {})
-     
     }
-
-
 }
+
+
+ 
